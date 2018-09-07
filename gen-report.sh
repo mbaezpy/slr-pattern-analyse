1e539c7126228e5dc84b7548a9848f6174762086
Marcos Baez <mbaezpy@gmail.com>
2018-09-07 16:15:13 +0200

    added custom output folder

--- gen-report.sh
+++ gen-report.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PAT_TYPE=$2
 CSV_FILE=$1
+NOW=`date "+%Y-%m-%d_%H.%M.%S"`
 
 if [ "$#" -ne 2 ]; then
     echo "Invalid number of arguments"
@@ -9,12 +10,12 @@ if [ "$#" -ne 2 ]; then
     exit 1
 fi
 
+mkdir -p output
 
 if [ $PAT_TYPE = "sentences" ]; then
-  Rscript -e 'library(rmarkdown); rmarkdown::render("./src/report-tmpl-sentence.Rmd", "html_document")' $CSV_FILE
-elif [ $PAT_TYPE = "keywords" ]; then
-
-  Rscript -e 'library(rmarkdown); rmarkdown::render("./src/report-tmpl-keywords.Rmd", "html_document")' $CSV_FILE
+  Rscript -e "library(rmarkdown); rmarkdown::render(\"./src/report-tmpl-sentence.Rmd\", \"html_document\", output_dir = \"./output/\", output_file =\"${PAT_TYPE}_$NOW.html\")" $CSV_FILE
+elif [ $PAT_TYPE = "keywords" ]; then  
+  Rscript -e "library(rmarkdown); rmarkdown::render(\"./src/report-tmpl-keywords.Rmd\", \"html_document\", output_dir = \"./output/\", output_file =\"${PAT_TYPE}_$NOW.html\")" $CSV_FILE
 else
     echo "Invalid type of pattern."
     echo "TYPE_PATTERN: keywords | sentences"
